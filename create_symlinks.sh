2dda2098d7a7014e856cc8348a26f593fda7e673
jackal <jack@lavnet.net>
2025-04-24 14:04:01 -0500

    working documentation

--- create_symlinks.sh
+++ create_symlinks.sh
@@ -1,4 +1,8 @@
 #!/bin/bash
+#
+#  This is used to make the playbooks work on a non-developers machine
+#  NOTE: you will need to remove the symbolic link for molecule testing
+#
 
 # Create the ansible collections directory structure if it doesn't exist
 mkdir -p ~/.ansible/collections/ansible_collections/jackaltx
